d4ef206b94433345b697358526dacfd8c67cb814
Emiri Sato <emirisato.1003@gmail.com>
2025-04-28 13:11:37 -0700

    setup airtable base

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,70 @@
 import TodoList from './features/TodoList/TodoList';
 import TodoForm from './features/TodoForm';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
 
-  function handleAddTodo(newTodo) {
+  const url = `https://api.airtable.com/v0/${import.meta.env.VITE_BASE_ID}/${import.meta.env.VITE_TABLE_NAME}`;
+  const token = `Bearer ${import.meta.env.VITE_PAT}`;
+
+  // create useEffect and fetching data
+  useEffect(() => {
+    const fetchTodos = async () => {
+      setIsLoading(true);
+
+      const options = {
+        method: "GET",
+        // body: JSON.stringify()
+        headers: {
+          // "Content-Type": "application.json",
+          "Authorization": token
+        }
+      };
+      // set up try/catch/finally
+      try {
+        const res = await fetch(url, options);
+        if (!res.ok) {
+          throw new Error(res.status);
+        }
+
+        const { records } = await res.json();
+
+        setTodoList(records.map(record => {
+          const todo = {
+            id: record.id,
+            title: record.title,
+            isCompleted: record.isCompleted,
+            ...record.fields,
+          };
+          if (!todo.isCompleted) {
+            todo.isCompleted = false;
+          }
+          return todo;
+        }));
+
+        // if (data.error) {
+        //   throw new Error(data.status);
+        // }
+        console.log(records);
+
+      } catch (error) {
+        console.log(error.message);
+        setErrorMessage(error.message);
+      } finally {
+        console.log('action completed');
+        setIsLoading(false);
+      }
+    };
+    fetchTodos();
+  }, []);
+
+  function handleAddTodo(title) {
     const todoTask = {
-      title: newTodo,
+      title: title,
       id: Date.now(),
       isCompleted: false
     };
@@ -25,22 +81,26 @@ function App() {
     setTodoList(updateTodos);
   }
 
-  function updateTodo(editedTodo){
+  function updateTodo(editedTodo) {
     const updateTodos = todoList.map(todo => {
-      if(todo.id === editedTodo.id){
+      if (todo.id === editedTodo.id) {
         console.log(editedTodo.title);
-        return{...editedTodo}
+        return { ...editedTodo };
       }
-      return todo
-    })
-    setTodoList(updateTodos)
+      return todo;
+    });
+    setTodoList(updateTodos);
   }
 
   return (
     <div>
       <h1>My Todos</h1>
       <TodoForm onAddTodo={handleAddTodo} />
-      <TodoList todoList={todoList} onCompleteTodo={onCompleteTodo} onUpdateTodo={updateTodo}/>
+      <TodoList
+        todoList={todoList}
+        onCompleteTodo={onCompleteTodo}
+        onUpdateTodo={updateTodo}
+        isLoading={isLoading} />
     </div>
   );
 }
