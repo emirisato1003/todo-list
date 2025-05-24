9792b17d29d9ea19c235d93e0764217a6415dab8
Emiri Sato <emirisato.1003@gmail.com>
2025-05-23 22:28:39 -0700

    create a reducer function

--- src/reducers/todos.reducer.js
+++ src/reducers/todos.reducer.js
@@ -1,15 +1,42 @@
 
 const ACTIONS = {
-    FETCH_TODO : 'fetchTodos', 
-    LOAD_TODOS : 'loadTodos',
-    SET_LOAD_ERROR : 'setLoadError',
+    FETCH_TODO: 'fetchTodos',
+    LOAD_TODOS: 'loadTodos',
+    SET_LOAD_ERROR: 'setLoadError',
     START_REQUEST: 'startRequest',
-    ADD_TODO : 'addTodo',
+    ADD_TODO: 'addTodo',
     END_REQUEST: 'endRequest',
     UPDATE_TODO: 'updateTodo',
     COMPLETE_TODO: 'completeTodo',
     REVERT_TODO: 'revertTodo',
     CLEAR_ERROR: 'clearError'
+};
+
+function reducer(state = initialState, action) {
+    switch (action.type) {
+        case ACTIONS.FETCH_TODO:
+            return { ...state, };
+        case ACTIONS.LOAD_TODOS:
+            return { ...state };
+        case ACTIONS.SET_LOAD_ERROR:
+            return { ...state };
+        case ACTIONS.START_REQUEST:
+            return { ...state };
+        case ACTIONS.ADD_TODO:
+            return { ...state };
+        case ACTIONS.END_REQUEST:
+            return { ...state };
+        case ACTIONS.UPDATE_TODO:
+            return { ...state };
+        case ACTIONS.COMPLETE_TODO:
+            return { ...state };
+        case ACTIONS.REVERT_TODO:
+            return { ...state };
+        case ACTIONS.CLEAR_ERROR:
+            return { ...state };
+        default:
+            return state;
+    }
 }
 
 const initialState = {
